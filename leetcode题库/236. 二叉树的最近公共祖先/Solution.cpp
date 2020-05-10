6f5afbc0ab6ef6f45a68c779ca14908972799fc5
eren <574246399@qq.com>
2020-05-10 09:04:26 +0800

    Update Solution.cpp

--- leetcode题库/236. 二叉树的最近公共祖先/Solution.cpp
+++ leetcode题库/236. 二叉树的最近公共祖先/Solution.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root)
-            return nullptr;
-        // 找到其中一个节点的父亲，因为值不唯一，所以不会出现重复的
-        if(p == root || q == root)
+		// 递归结束条件，如果为空返回空；如果找到了，则返回该节点
+        if(!root || p == root || q == root)
             return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        // 说明在右边
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+
         if(!left)
             return right;
         if(!right)
             return left;
-        // 一边一个
-        if(left && right)
-            return root;
-        // 语法原因，因为肯定存在，所以不会走到
-        return nullptr;
+        return root;
     }
 };
